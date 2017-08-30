2f2efd422eb7b948093e0568734eb2b83535c0a7
xiaotian <wanghuitian2011@163.com>
2017-08-30 23:34:31 +0800

    改http为https

--- song.js
+++ song.js
@@ -18,7 +18,7 @@ $(function(){
     })
   }) 
   let audio = document.createElement('audio')
-  audio.src="http://dl.stream.qqmusic.qq.com/C4000027zPYs3A9fyb.m4a?fromtag=38&vkey=DAF062B24BB951B4561F9445BA2C4D7D56FFF804AB096CAC924F65118014E3AB80981DEC60112EBBFEDBD78E28E9EC9A69F71CE52FDFC775&guid=1981527532"
+  audio.src="https://dl.stream.qqmusic.qq.com/C4000027zPYs3A9fyb.m4a?fromtag=38&vkey=DAF062B24BB951B4561F9445BA2C4D7D56FFF804AB096CAC924F65118014E3AB80981DEC60112EBBFEDBD78E28E9EC9A69F71CE52FDFC775&guid=1981527532"
   audio.oncanplay = function(){
       audio.play()
       $('.disc-container').addClass('playing')
